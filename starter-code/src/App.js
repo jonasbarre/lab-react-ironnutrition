8356416a15040adb3aa174bb4c7de8a884fa5963
Jonas <jonas@smartvizual.com>
2020-04-30 21:41:42 +0200

    Iterations 1-3
    Co-authored-by: anastezia-kz <anastasiia.sh.v@gmail.com>
    Co-authored-by: Rana Marques <gutaraposomarques@gmail.com>

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.css';
+import foods from './foods.json';
+import FoodBox from './components/FoodBox';
 
 class App extends Component {
+
+  state = {
+    name: 'a food',
+    calories: '123',
+    image: 'https://img.icons8.com/bubbles/2x/food.png',
+    visibility: false
+  }
+
+
+  // handleInput = (e) => {
+  //   let { name, value } = e.target
+  //   this.setState({ [name]: value })
+  // }
+
+  // handleSubmit = (e) => {
+  //   e.preventDefault()
+  //   const newStories = [...this.state.stories]
+  //   newStories.push({
+  //     name: this.state.name,
+  //     calories: this.state.calories
+  //   })
+  //   this.setState({ stories: newStories })
+  //   console.log(this.state)
+  // }
+  
+  showForm = ()=>{
+    this.setState({
+      ...this.state,
+      visibility: !this.state.visibility
+    })
+    console.log(this.state.visibility)
+   }
+  
   render() {
+    console.log(foods[0].image)
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button onClick={this.showForm}>CLICK ME</button>
+        <form onSubmit={(e) => this.handleSubmit(e)} className={(this.state.visibility === true)?'triggered':'hidden'}>
+          <input type="text" name="name" value={this.state.name}  />
+          <input type="number" name="calories" value={this.state.calories}  />
+          <input type="text" name="image" value={this.state.image}  />
+          <input type="submit" value="submit" />
+        </form>
+
+        {foods.map((food, index) => (
+          <FoodBox
+            key={index}
+            name={food.name}
+            image={food.image}
+            calories={food.calories}
+            quantity={food.quantity}
+          />
+        ))}
+
       </div>
     );
   }
